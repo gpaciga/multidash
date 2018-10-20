98b154317d177a01bebc279810800fea06050d52
Gregory Paciga <gpaciga@gmail.com>
2018-10-20 14:53:07 -0400

    use styled components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Multidash
-          </p>
-        </header>
-      </div>
+      <AppWrapper>
+        Multidash  
+      </AppWrapper>  
     );
   }
 }
 
 export default App;
+
