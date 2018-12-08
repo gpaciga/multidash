ec8939f3dde623f67e5a606fc88fc41ed94ca73b
Gregory Paciga <gpaciga@gmail.com>
2018-12-08 14:28:23 -0500

    image from url in example config

--- src/config.js
+++ src/config.js
@@ -5,6 +5,11 @@ export const SLIDES = [
   {
     type: "url",
     path: "http://example.com",
+    seconds: 5
+  }, {
+    type: "img",
+    path: "http://via.placeholder.com/300",
+    seconds: 5
   }, {
     type: "img",
     path: "images/puppy.jpg",
